e14fff6e6405e8a3a45ae3c69e67dc1c8e812eae
Nitin saini <nitin.saini@softobiz.com>
2024-09-23 18:00:44 +0530

    swagger added

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -21,29 +21,29 @@ console.info('Current Environment: ' + currentEnv);
       load: loadConfig,
       envFilePath: path.resolve(process.cwd(), 'env', `.env.${currentEnv}`),
     }),
-    I18nModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        fallbackLanguage: configService.getOrThrow('app.fallbackLanguage', {
-          infer: true,
-        }),
-        loaderOptions: { path: path.join(__dirname, '../i18n/'), watch: true },
-      }),
-      resolvers: [
-        {
-          use: HeaderResolver,
-          useFactory: (configService: ConfigService) => {
-            return [
-              configService.get('app.headerLanguage', {
-                infer: true,
-              }),
-            ];
-          },
-          inject: [ConfigService],
-        },
-      ],
-      imports: [ConfigModule],
-      inject: [ConfigService],
-    }),
+    // I18nModule.forRootAsync({
+    //   useFactory: (configService: ConfigService) => ({
+    //     fallbackLanguage: configService.getOrThrow('app.fallbackLanguage', {
+    //       infer: true,
+    //     }),
+    //     loaderOptions: { path: path.join(__dirname, '../i18n/'), watch: true },
+    //   }),
+    //   resolvers: [
+    //     {
+    //       use: HeaderResolver,
+    //       useFactory: (configService: ConfigService) => {
+    //         return [
+    //           configService.get('app.headerLanguage', {
+    //             infer: true,
+    //           }),
+    //         ];
+    //       },
+    //       inject: [ConfigService],
+    //     },
+    //   ],
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    // }),
     EventEmitterModule.forRoot()
   ]
 })
