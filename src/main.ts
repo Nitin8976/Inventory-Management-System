9279011673b9ff1fbb70e09378a1c59165e42d1f
Nitin8976 <dcsa2022nitin@kuk.ac.in>
2024-09-23 18:34:27 +0530

    swagger included

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {bodyParser: true});
+const configService = app.get(ConfigService)
+app.setGlobalPrefix('v1')
+
+if (configService.get('app.isDocApiEnabled')){
+  const config = new DocumentBuilder()
+    .setTitle(configService.get('app.docApiTitle'))
+    .setDescription(configService.get('app.docApiDesc'))
+    .setVersion(configService.get('app.docApiV'))
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(configService.get('app.docApiBaseUrl'),app, document,{
+    swaggerOptions:{
+      persistAuthorization:true
+    }
+  });
+}
+
+// app.useGlobalFilters(new HttpExceptionFilter());
+// app.useGlobalPipes(new ValidationPipe({transform :true}))
+  await app.listen(configService.get('app.port'));
 }
 bootstrap();
