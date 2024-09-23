d7159c005f0bf416d40f5bc5c606afb67baf9b55
Nitin saini <nitin.saini@softobiz.com>
2024-09-23 18:09:22 +0530

    removed something

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bodyParser: true });
-  const configService = app.get(ConfigService);
-
-  // Swagger api documentation
-  if (configService.get('app.isDocApiEnabled')) {
-    const config = new DocumentBuilder()
-      .setTitle(configService.get('app.docApiTitle'))
-      .setDescription(configService.get('app.docApiDesc'))
-      .setVersion(configService.get('app.docApiV'))
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-
-    await app.listen(3000);
-  }
-  bootstrap();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
+bootstrap();
